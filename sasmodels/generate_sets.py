aec2d6246402456e14c988b7751c6224651df19b
Chris Wang <christopher.wang@nist.gov>
2017-07-25 14:40:31 -0400

    Rewrite argument parsing for generate_sets
    Signed-off-by: Chris Wang <christopher.wang@nist.gov>

--- sasmodels/generate_sets.py
+++ sasmodels/generate_sets.py
@@ -9,6 +9,7 @@ directory. See example_data.dat for a sample of the file format used.
 """
 from __future__ import print_function
 
+import argparse
 import os
 import resource
 import sys
@@ -17,6 +18,7 @@ import traceback
 from collections import OrderedDict, namedtuple
 
 import numpy as np  # type: ignore
+import psycopg2 as pgsql
 
 from . import core
 from .compare import (randomize_pars, suppress_pd, make_data,
@@ -27,11 +29,44 @@ MODELS = core.list_models()
 
 # Names for columns of data gotten from simulate
 DATA_NAMES = ["Q", "dQ", "I(Q)", "dI(Q)"]
+conn = pgsql.connect(
+    "dbname=sas_data user=sasnets password=sasnets host=127.0.0.1")
+
+parser = argparse.ArgumentParser(prog="generate_sets.py",
+                                 description="A script that generates SANS "
+                                             "datasets for use in neural "
+                                             "network training.")
+parser.add_argument("model",
+                    help="model is the model name of the model or one of the "
+                         "model types listed in sasmodels.core.list_models (all"
+                         ", py, c, double, single, open cl, 1d, 2d, nonmagnetic"
+                         ", magnetic).  Model types can be combined, such as 2d"
+                         "+single.")
+parser.add_argument("count", type=int,
+                    help="count is the number of distinct models to generate.")
+parser.add_argument("dimension", choices=['1D', '2D'],
+                    help="Choose whether to generate 1D or 2D data.")
+parser.add_argument("npoint", type=int, help="The number of points per model.")
+parser.add_argument("cutoff",
+                    help="CUTOFF is the cutoff value to use for the "
+                         "polydisperse distribution. Weights below the cutoff "
+                         "will be ignored. Use 'mono' for monodisperse models. "
+                         "The choice of polydisperse parameters, and the number"
+                         " of points in the distribution is set in compare.py "
+                         "defaults for each model. Polydispersity is given in "
+                         "the 'demo' attribute of each model.")
+parser.add_argument("precision",
+                    choices=['single', 'double', 'fast', 'single!', 'double!',
+                             'quad!'],
+                    help="Precision to use in floating point calculations. "
+                         "If postfixed with an '!', builds a DLL for the CPU.")
+parser.add_argument("-v", "--verbose", help="Verbose output level.",
+                    choices=[0, 1, 2])
 
 
 # noinspection PyTypeChecker
 def gen_data(name, data, index, N=1, mono=True, cutoff=1e-5,
-             base='sasview', output_dir='out_all2/'):
+             base='sasview', output_dir='out_all3/'):
     """
     Generates the data for the given model and parameters.
 
@@ -110,14 +145,15 @@ def gen_data(name, data, index, N=1, mono=True, cutoff=1e-5,
         while True:
             pars_i.update({'scale': 1, 'background': 1e-5})
             dat = np.vstack(exec_model(calc_base, pars_i))
-            if np.in1d(dat, np.NaN).any() or np.in1d(dat, np.Inf).any():
+            if np.in1d(dat, np.NaN).any() or np.in1d(dat, np.Inf).any() or len(
+                    dat[dat <= 0]) > 0:
                 del dat
                 # print("Maths error, retrying")
                 count += 1
             else:
                 break
             if count > 2000:
-                print("Maths error; bad model: ", name)
+                logging.error("Maths error; bad model: "+name)
                 return
         columns = [v for _, v in sorted(pars_i.items())]
         result_dict = OrderedDict()
@@ -131,30 +167,29 @@ def gen_data(name, data, index, N=1, mono=True, cutoff=1e-5,
         result_dict["data"] = data_dict
 
         iq.append(dat.T[2].tolist())
+        diq.append(dat.T[3].tolist())
+        mname.append(name)
         if first:
-            mname.append(name)
-            mname.append(N)
             q = dat.T[0].tolist()
             dq = dat.T[1].tolist()
-            diq = dat.T[3].tolist()
             first = False
     if not os.path.exists(os.path.dirname(output_dir)):
         os.makedirs(os.path.dirname(output_dir))
-    with open(output_dir + 'result_eval_' + name, 'w') as fd:
-        fd.write(str(mname) + '\n')
-        fd.write(str(q) + '\n')
-        fd.write(str(iq) + '\n')
-        fd.write(str(dq) + '\n')
-        fd.write(str(diq) + '\n')
+    # with open(output_dir + 'result_eval_' + name, 'w') as fd:
+    #    fd.write(str(mname) + '\n')
+    #    fd.write(str(q) + '\n')
+    #    fd.write(str(iq) + '\n')
+    #    fd.write(str(dq) + '\n')
+    #    fd.write(str(diq) + '\n')
+    with conn:
+        with conn.cursor() as d:
+            for a, b, c in zip(iq, diq, mname):
+                d.execute(
+                    "INSERT INTO new_eval_data (iq, diq, model) VALUES (%s, %s, %s)",
+                    (a, b, c))
+    conn.commit()
+    d.close()
     print("Complete")
-
-
-def print_usage():
-    """
-    Print the command usage string.
-    """
-    print("usage: generate_sets.py MODEL COUNT (1dNQ|2dNQ) (CUTOFF|mono) "
-          "(single|double|quad)", file=sys.stderr)
 
 
 def print_models():
@@ -164,53 +199,15 @@ def print_models():
     print(columnize(MODELS, indent="  "))
 
 
-def print_help():
-    """
-    Print usage string, the option description and the list of available models.
-    """
-    print_usage()
-    print("""\
-
-MODEL is the model name of the model or one of the model types listed in
-sasmodels.core.list_models (all, py, c, double, single, opencl, 1d, 2d,
-nonmagnetic, magnetic).  Model types can be combined, such as 2d+single.
-$
-COUNT is the number of randomly generated parameter sets to try. A value
-of "10000" is a reasonable check for monodisperse models, or "100" for
-polydisperse models.   For a quick check, use "100" and "5" respectively.
-
-NQ is the number of Q values to calculate.  If it starts with "1d", then
-it is a 1-dimensional problem, with log spaced Q points from 1e-3 to 1.0.
-If it starts with "2d" then it is a 2-dimensional problem, with linearly
-spaced points Q points from -1.0 to 1.0 in each dimension. The usual
-values are "1d100" for 1-D and "2d32" for 2-D.
-
-CUTOFF is the cutoff value to use for the polydisperse distribution. Weights
-below the cutoff will be ignored.  Use "mono" for monodisperse models.  The
-choice of polydisperse parameters, and the number of points in the distribution
-is set in compare.py defaults for each model.  Polydispersity is given in the
-"demo" attribute of each model.
-
-PRECISION is the floating point precision to use for comparisons. Precision is 
-one of fast, single, double for GPU or single!, double!, quad! for DLL.  If no
-precision is given, then this defaults to single and double! respectively.
-
-Available models:
-""")
-    print_models()
-
-
 def main(argv):
     """
     Main program.
     """
-    if len(argv) not in (3, 4, 5):
-        print_help()
-        return
+
     dat = namedtuple('Data', ['q', 'I', 'dI', 'dq', 'meanQ', 'shadow'])
     d = dat(*np.loadtxt("testFile1.ABS", skiprows=5).T)
     # print(len(core.list_models("all")))
-    #for x in ['adsorbed_layer', 'barbell', 'bcc_paracrystal',
+    # for x in ['adsorbed_layer', 'barbell', 'bcc_paracrystal',
     #          'be_polyelectrolyte', 'binary_hard_sphere', 'broad_peak',
     #          'capped_cylinder', 'core_multi_shell', 'core_shell_bicelle',
     #          'core_shell_bicelle_elliptical', 'core_shell_cylinder',
@@ -232,32 +229,38 @@ def main(argv):
     #          'squarewell', 'stacked_disks', 'star_polymer', 'stickyhardsphere',
     #          'surface_fractal', 'teubner_strey', 'triaxial_ellipsoid',
     #          'two_lorentzian', 'two_power_law', 'unified_power_Rg', 'vesicle']:
+    # print(len(core.list_models("all")))
     for x in [#'adsorbed_layer', 'barbell', 'bcc_paracrystal',
-              #'be_polyelectrolyte', 'binary_hard_sphere', 'broad_peak',
-              #'capped_cylinder', 'core_multi_shell', 'core_shell_bicelle',
-              #'core_shell_bicelle_elliptical', 'core_shell_cylinder',
-              #'core_shell_ellipsoid', 'core_shell_parallelepiped',
-              #'core_shell_sphere', 'correlation_length', 'cylinder', 'dab',
-              #'ellipsoid', 'elliptical_cylinder', 'flexible_cylinder',
-              #'flexible_cylinder_elliptical', 'fractal', 'fractal_core_shell',
-              #'fuzzy_sphere', 'gauss_lorentz_gel', 'gaussian_peak', 'gel_fit',
-              #'guinier', 'guinier_porod', 'hardsphere', 'hayter_msa',
-              #'binary_hard_sphere','broad_peak',
-              'hollow_cylinder', 'hollow_rectangular_prism',
-              'hollow_rectangular_prism_thin_walls', 'lamellar', 'lamellar_hg',
-              'lamellar_hg_stack_caille', 'lamellar_stack_caille',
-              'lamellar_stack_paracrystal', 'line', 'linear_pearls', 'lorentz',
-              'mass_fractal', 'mass_surface_fractal', 'mono_gauss_coil',
-              'multilayer_vesicle', 'onion', 'parallelepiped', 'peak_lorentz',
-              'pearl_necklace', 'poly_gauss_coil', 'polymer_excl_volume',
-              'polymer_micelle', 'porod', 'power_law', 'pringle',
-              'raspberry',
-              'rectangular_prism', 'rpa', 'sphere', 'spherical_sld', 'spinodal',
-              'squarewell', 'stacked_disks', 'star_polymer', 'stickyhardsphere',
-              'surface_fractal', 'teubner_strey', 'triaxial_ellipsoid',
-              'two_lorentzian', 'two_power_law', 'unified_power_Rg', 'vesicle']:
-         run_model(x, argv, dq1=d)
-    #run_model(argv[0], argv, dq1=d)
+             'be_polyelectrolyte', 'binary_hard_sphere', #'broad_peak',
+             #'capped_cylinder', 'core_multi_shell', 'core_shell_bicelle',
+             #'core_shell_bicelle_elliptical', 'core_shell_cylinder',
+             # 'core_shell_ellipsoid', 'core_shell_parallelepiped',
+             # 'core_shell_sphere', 'correlation_length', 'cylinder', 'dab',
+             # 'ellipsoid', 'elliptical_cylinder',
+             #'flexible_cylinder',
+             #'flexible_cylinder_elliptical',
+             'fractal', 'fractal_core_shell',
+             #'fuzzy_sphere', 'gauss_lorentz_gel', 'gaussian_peak', 'gel_fit',
+             #'guinier', 'guinier_porod', 'hardsphere',
+             'hayter_msa',
+             #'hollow_cylinder',
+             'hollow_rectangular_prism',
+             #'hollow_rectangular_prism_thin_walls', 'lamellar', 'lamellar_hg',
+             #'lamellar_hg_stack_caille', 'lamellar_stack_caille',
+             #'lamellar_stack_paracrystal', #'line', 'linear_pearls', 'lorentz',
+             'mass_fractal', #'mass_surface_fractal', 'mono_gauss_coil',
+             #'multilayer_vesicle', 'onion', 'parallelepiped', 'peak_lorentz',
+             #'pearl_necklace', #'poly_gauss_coil', 'polymer_excl_volume',
+             #'polymer_micelle', #'porod', 'power_law', 'pringle', 'raspberry',
+             #'rectangular_prism',
+             'rpa', #'sphere', 'spherical_sld', 'spinodal',
+             'squarewell', #'stacked_disks', 'star_polymer',
+             'stickyhardsphere',
+             #'surface_fractal', 'teubner_strey', 'triaxial_ellipsoid',
+             #'two_lorentzian', 'two_power_law', 'unified_power_Rg', 'vesicle'
+             ]:
+        run_model(x, parsed, dq1=d)
+    #run_model(parsed.model, parsed, dq1=d)
 
 
 def run_model(model, argv, dq1=None):
@@ -270,22 +273,18 @@ def run_model(model, argv, dq1=None):
         print('model types: all, py, c, double, single, opencl, 1d, 2d, '
               'nonmagnetic, magnetic')
         return
-    try:
-        count = int(argv[1])
-        is2D = argv[2].startswith('2d')
-        assert argv[2][1] == 'd'
-        nq = int(argv[2][2:])
-        mono = len(argv) <= 3 or argv[3] == 'mono'
-        cutoff = float(argv[3]) if not mono else 0
-        base = argv[4] if len(argv) > 4 else "single"
-    except Exception:
-        traceback.print_exc()
-        print_usage()
-        return
+    count = argv.count
+    is2D = argv.dimension.startswith('2d')
+    nq = argv.npoint
+    mono = argv.cutoff == 'mono'
+    cutoff = float(argv.cutoff) if not mono else 0
+    base = argv.precision
+
     data, index = make_data({'qmax': 1.0, 'is2d': is2D, 'nq': nq, 'res': 0.03,
                              'accuracy': 'Low', 'view': 'log', 'zero': False})
     if dq1 is not None:
         data.dx = dq1.dq
+        data.x = dq1.q
     for model in model_list:
         gen_data(model, data, index, N=count, mono=mono,
                  cutoff=cutoff, base=base)
@@ -298,10 +297,11 @@ if __name__ == "__main__":
     import logging
 
     logging.basicConfig(level=logging.INFO)
+    parsed = parser.parse_args(sys.argv[1:])
     time_start = time.clock()
-    main(sys.argv[1:])
+    main(parsed)
     time_end = time.clock() - time_start
     print('Total computation time (s): %.2f' % (time_end / 10))
     print('Total memory usage: %.2f' %
           resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
-    # Units are OS dependent
+    # Units of mem are OS dependent
